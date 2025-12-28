81208c3f271361fb43ae9bf69869d7485261bdd4
sachinkr0872 <sachinkr0872@gmail.com>
2025-12-28 20:47:45 +0530

    feat:add math object and variables

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+let secretNumber = Math.floor(Math.random() * 100) + 0;
+let attempts = 0;
+
+const input = document.getElementById("guessInput");
+const message = document.getElementById("message");
+const attemptsText = document.getElementById("attempts");
+
+
 
